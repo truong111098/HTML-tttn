8c85fc111bc19f41812f23ebcee262176b1d86bd
Võ Quang Trường <56054738+truong111098@users.noreply.github.com>
2020-07-13 08:57:04 +0700

    Add files via upload

---
+++ myView.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>shopf8</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="{{url('./css/style.css')}}">
+	<style type="text/css">
+		body{
+			font-family: Tahoma;
+			font-size: 13px;
+			width: 1000px;
+			margin: 0px auto;
+		}
+		#banner{
+			background: ;
+			height: auto;
+			width: 1000px
+		}
+		#banner img{
+			width: 1000px;
+		}
+		#ten{
+			text-align: center;
+			background: white;
+			height: 50px;
+			width: 1000px
+		}
+		#menu{
+			background: white;
+			height: 50px;
+			width: 1000px;
+		}
+		#main #left{
+			border: 2px;
+			width: 400px;
+			background: white;
+			float: left;
+			min-height: 400px;
+		}
+		#main #right{
+			border: 2px;
+			width: 600px;
+			background: white;
+			min-height: 400px;
+			float: left;
+		}
+		#footer{
+			background: #000;
+			width: 1000px;
+			text-align: center;
+			color: white;
+		}
+		#menu ul{
+			margin: 0px;
+			padding: 0px;
+		}
+		#menu ul li{
+			list-style: none;
+			float: left;
+			padding: 0px 15px 0px 15px;
+			line-height: 40px;
+		}
+		#main #right ul{
+			margin: 0px;
+			padding: 0px;
+		}
+		#main #right ul li{
+			list-style: none;
+		}
+		#main #right ul li img{
+			float: left;
+		}
+
+
+
+	</style>
+</head>
+<body>
+
+	<div id="banner"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p280x280/107619293_610769196517842_6106862977506025362_n.png?_nc_cat=111&_nc_sid=b96e70&_nc_ohc=Rspv7V51vbEAX_3XLnz&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=3ec28ebaa4af4a0765d27e53bddeaff2&oe=5F30B58B"></div>
+			<div id="ten"><h1>Huyền Em</h1></div>
+		<div id="menu">
+			<ul>
+				<li>Dòng Thời Gian</li>
+				<li>Giới thiệu</li>
+				<li>Bạn bè</li>
+				<li>Ảnh</li>
+				<li>khác</li>
+				<li>Chỉnh sửa trang cá nhân</li>
+			</ul>
+		</div>
+
+	<div id="main">
+		<div id="left"> 
+			<ul>
+			<h1>giới thiệu</h1>
+			<li><h4>Sống tại Vinh</h4></li>
+			<li><h4>Đang thực tập và làm việc tại cty Hachinet</h4></li>
+			<li><h4>có 1999 người theo dõi</h4>	</li>
+			<li><h4 style="align-content: center;">Ảnh đáng chú ý</h4></li>
+			</ul>
+			<img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/109456496_691672444726541_5466751654452273436_n.png?_nc_cat=110&_nc_sid=b96e70&_nc_ohc=OS6np5upwfoAX--k1s8&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=e5a4c95d834ce76c97daaefb89ba4aef&oe=5F3218EE">
+		</div>
+		<div id="right">
+			<ul>
+				<li>
+					<img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/107866420_382525336055106_5664669786254046347_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=uylvrl7VEQsAX_URONY&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=bfcbd58d559a74d57ca06495f1017198&oe=5F300166">
+					<h2>Huyền Em</h2>
+					<p style="font-size: 10px">1 giờ trước</p>
+				</li>
+				<p>	Có những ngày buồn tủi đến lạ </p>
+				<p>chỉ muốn vứt bỏ tất cả để chạy ngay về nhà
+					 sà vào lòng bố mẹ rồi khóc nức nở...</p>
+				<img style="height: 250px" src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p280x280/107525642_1728655470627122_709278354093369668_n.png?_nc_cat=104&_nc_sid=b96e70&_nc_ohc=TsiVOhaHsCsAX_L197Y&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=a92e5683c6ac36ac8bbaf17a29fb7dcd&oe=5F2FFAEB">
+					
+			</ul>
+		</div>
+	</div>
+	<div id="footer">footer</div>
+</body>
+</html>
